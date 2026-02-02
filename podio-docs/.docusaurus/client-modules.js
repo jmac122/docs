d54f2ef98eeb4616c28376c18cbea9ed81f3e585
jmac122 <jmckenzie@precisionsiteservices.com>
2026-02-02 20:17:43 +0000

    Add root .gitignore and remove build artifacts from tracking
    - Add comprehensive .gitignore at repo root
    - Remove podio-docs/build/, node_modules/, .docusaurus/ from git tracking
    - These are generated files that shouldn't be versioned

--- podio-docs/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/home/jmckenzie/clawd/jmac122-docs/podio-docs/node_modules/infima/dist/css/default/default.css"),
-  require("/home/jmckenzie/clawd/jmac122-docs/podio-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/jmckenzie/clawd/jmac122-docs/podio-docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/jmckenzie/clawd/jmac122-docs/podio-docs/src/css/custom.css"),
-];
--- podio-docs/build/assets/js/aba21aa0.054e9895.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunkpodio_docs=globalThis.webpackChunkpodio_docs||[]).push([[742],{7093(o){o.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
